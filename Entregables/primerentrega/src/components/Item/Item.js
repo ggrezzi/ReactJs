eb45b387c447e97a0a738902b3ff9c90db237263
Gabriel <ggrezzi@tlcorporate.com>
2023-07-05 09:10:30 -0300

    indentando correctamente

--- Entregables/primerentrega/src/components/Item/Item.js
+++ Entregables/primerentrega/src/components/Item/Item.js
@@ -4,33 +4,32 @@ import { Link } from "react-router-dom"
 const Item = ({id, name, img, price, stock})=>{
     return (
 
-        <>
-        <div class='column is-one-quarter'>
-        
-        <div class="card equal-height">
-            <div class="card-image has-text-centered">
-                <figure class="image is-64x64 is-inline-block">
-                    <img src={img} alt={name} />
-                </figure>
-            </div>
-            <div class="card-content">
-                <div class="media">
-                    <div class="media-left">
+        <div className="contenedor">
+            <div class='column is-one-quarter'>
+                <div class="card equal-height">
+                    <div class="card-image has-text-centered">
+                        <figure class="image is-64x64 is-inline-block">
+                            <img src={img} alt={name} />
+                        </figure>
                     </div>
-                    <div class="media-content">
-                        <p class="title is-4">{name}</p>
-                        <p class="subtitle is-6">Precio: ${price}</p>
-                        <p class="subtitle is-6">Stock Disponible: {stock}</p>
-                    </div>
-                </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-left">
+                            </div>
+                            <div class="media-content">
+                                <p class="title is-4">{name}</p>
+                                <p class="subtitle is-6">Precio: ${price}</p>
+                                <p class="subtitle is-6">Stock Disponible: {stock}</p>
+                            </div>
+                        </div>
 
-                <div class="content">
-                    <Link to={`/item/${id}`} className="option" class="button is-primary">Ver detalle</Link>
+                        <div class="content">
+                            <Link to={`/item/${id}`} className="option" class="button is-primary">Ver detalle</Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        </div></>
-        
     )
 }
 
