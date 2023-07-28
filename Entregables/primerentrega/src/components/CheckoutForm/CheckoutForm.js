2fd198d5e3079cacfc02e3c38684d4e166c64570
Gabriel <ggrezzi@tlcorporate.com>
2023-07-28 18:35:36 -0300

    Actualizando para que no acepte la compra si el email de verificacion no es el mismo que el email

--- Entregables/primerentrega/src/components/CheckoutForm/CheckoutForm.js
+++ Entregables/primerentrega/src/components/CheckoutForm/CheckoutForm.js
@@ -2,31 +2,47 @@ import { useState } from "react";
 
 const CheckoutForm = ({onConfirm})=>{
     const [name, setName] = useState('')
+    const [lastName, setLastName] = useState('')
     const [phone, setPhone] = useState('')
     const [email, setEmail] = useState('')
+    const [emailVerification, setEmailVerification] = useState('')
 
     const handleConfirm = (event) => {
+
         event.preventDefault()
-        const userData = {
+        if (emailVerification!==email){
+            console.error('Verifica que el email y el email de confirmacion sean el mismo')
+            //SetError("error en validacion de emails")
+        }
+        else{const userData = {
             name, phone, email
         }
-        onConfirm(userData)
+        onConfirm(userData)}
+        
     }
 
     return(
         <div className="Container">
             <form onSubmit={handleConfirm} className="Form">
-                <label className="label">
+            <label className="label">
                     Nombre
-                    <input className="Input" type="text" value={name} onChange={({target}) => setName(target.value)}/>
+                    <input className="Input" type="text" value={name} onChange={({target}) => setName(target.value)} required/>
+                </label>
+                <label className="label">
+                    Apellido
+                    <input className="Input" type="text" value={lastName} onChange={({target}) => setLastName(target.value)} required/>
                 </label>
                 <label className="label">
                     Telefono
-                    <input className="Input" type="text" value={phone} onChange={({target}) => setPhone(target.value)}/>
+                    <input className="Input" type="text" value={phone} onChange={({target}) => setPhone(target.value)} required/>
                 </label>
                 <label className="label">
                     Email
-                    <input className="Input" type="email" value={email} onChange={({target}) => setEmail(target.value)}/>
+                    <input className="Input" type="email" value={email} onChange={({target}) => setEmail(target.value)} required/>
+                </label>
+                <label className="label">
+                    Verificacion Email
+                    <input className="Input" type="email" value={emailVerification}  onChange={({target}) => setEmailVerification(target.value)} required/>
                 </label>
                 <div className="Label">
                     <button type="submit" className="Button">Crear Pedido</button>
